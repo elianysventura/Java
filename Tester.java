1e5f773fcfa1e2cadf9359233469dfba994cb993
elianysventura <48776886+elianysventura@users.noreply.github.com>
2019-09-30 16:10:18 -0400

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,26 @@
+/**************************************************************
+ * Purpose/Description:This tests the findDominant method
+ * Author’s Panther ID:5652936
+ * Certification:I hereby certify that this work is my own and none of it is 
+ * the work of any other person.***********************************************
+ ***************/
+package cop3530.a1.p1;
+
+/**
+ * Class that Test the findDominant method
+ *
+ * @author elianys
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+        COP3530A1P1 p1 = new COP3530A1P1();//Creates object
+
+        int a[] = {16, 17, 4, 3, 5, 2};//Creates array
+        int n = a.length;//Variable n is the length of the array
+
+        p1.findDominant(a, n);/*Uses the findDominant method to get the dominant
+        numbers of the array*/
+
+    }
+}
